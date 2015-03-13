bfae03155f632e842576697e9169c5a82b97284a
Tom Rand <trand87@gmail.com>
2015-03-13 11:08:06 -0400

    transitions animations.

--- js/main.js
+++ js/main.js
@@ -9,10 +9,18 @@ function setFooterDimentions() {
 	$('.backTo-footer').css('width', window.innerWidth - 40 + 'px');
 }
 
+// Set the padding-top of the project header based on the body position
+function setProjectHeaderPadding() {
+	var bodyPosition = $('body').offset().top;
+	$('.project-header').css('padding-top', bodyPosition * 2);
+	$('.project-details').css('padding-top', bodyPosition * 2);
+}
+
 // What happens when the user resizes the browser...
 function onResizeEvent() {
 	$(window).resize(function() {
 		setHTMLDimentions();
+		setProjectHeaderPadding();
 		// setFooterDimentions();
 	})
 }
@@ -41,6 +49,12 @@ function toggleProjectDetails(project) {
 	$('.linked-list li').not(project).fadeToggle();
 	$(targetProject).animate({height:'toggle'});	
 	$('.frame').fadeToggle();
+	$('.linked-list .projectType-label').fadeToggle();
+	$('header').animate({height: 'toggle'}, function() {
+		
+	});
+
+	$(targetProject).find('.project-header').toggleClass('header-transformed');
 }
 
 // What happens when the user clicks on the menu icon...
@@ -60,6 +74,7 @@ function selectCloseIconEvent() {
 
 $(function initialize() {
 	setHTMLDimentions();
+	setProjectHeaderPadding();
 	// setFooterDimentions();
 	onResizeEvent();
 	selectProjectEvent();
